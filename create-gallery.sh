b92a0820b362515056aaae528e8c2dfebb02442d
Jerko Steiner <jerko.steiner@gmail.com>
2015-11-22 21:46:38 -0500

    Resized images and added single watermark

--- create-gallery.sh
+++ create-gallery.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 GALLERY_DIR="./public/gallery"
-IMAGES_DIR="../svena-images"
+RAW_IMAGES_DIR="../svena-images"
 THUMBS_DIR="./public/thumbs"
+IMAGES_DIR="./public/images"
 THUMB_SIZE="x400"
+IMAGE_SIZE="950x"
+WATERMARK='./watermark.png'
 
 function get_file_wo_ext {
     echo $1 | sed 's|^.*/||g' | sed 's|\..*$||g'
@@ -12,34 +15,46 @@ function create_templates {
     echo creating templates...
     mkdir -p "$GALLERY_DIR"
 
-    for file in "$IMAGES_DIR"/*; do
+    for file in "$RAW_IMAGES_DIR"/*; do
         file="../$(echo $file | sed 's|./public/||g')"
         # jade=$(echo $file | sed 's|^.*/||g' | sed 's|\..*$||g').jade
         jade=$(get_file_wo_ext $file).jade
         echo   creating template for $file as $jade
         ln -sr "$GALLERY_DIR/_image.jade" "$GALLERY_DIR/$jade"
-        # cp './public/_image.jade' "$GALLERY_DIR/$jade"
-        # cat ./public/_image.jade | sed "s|{{image}}|$file|g" > "$GALLERY_DIR/${jade,,}.jade"
     done
 
     echo templates done
+}
+
+function resize_images {
+    echo resizing images...
+    mkdir -p "$IMAGES_DIR"
+    for file in "$RAW_IMAGES_DIR"/*; do
+        echo "  resizing image $file"
+        image=$(get_file_wo_ext $file).jpg
+
+        composite -watermark 20% -gravity southeast "$WATERMARK" "$file" - | \
+            convert - -resize "$IMAGE_SIZE" -gaussian-blur 0x0.5 "$IMAGES_DIR/$image"
+    done
+
+    echo resizing done
 }
 
 function create_thumbnails {
     echo creating thumbnails...
     mkdir -p "$THUMBS_DIR"
 
-    for file in "$IMAGES_DIR"/*; do
+    for file in "$RAW_IMAGES_DIR"/*; do
         echo "  creating thumbs for $file"
         image=$(get_file_wo_ext $file).jpg
         convert "$file" -resize "$THUMB_SIZE" "$THUMBS_DIR/$image"
-        echo $file
     done
 
     echo thumbnails done
 }
 
 create_templates
+resize_images
 create_thumbnails
 
 echo done
